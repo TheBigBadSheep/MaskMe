547c4cef173e6e7562231192acb304c87d40c724
BigBadSheep <j.antich11@gmai.com>
2021-06-26 16:18:47 +0200

    Callouts

--- my-app/components/Map/MaskMarker.js
+++ my-app/components/Map/MaskMarker.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-import { Marker } from "react-native-maps";
-
+import { Marker, Callout } from "react-native-maps";
+import { View, Text } from "react-native";
 import mask from "../../Design/Icons/mask2.png";
 
 export default MaskMarker = ({ coordinate, title, description }) => {
@@ -9,8 +9,22 @@ export default MaskMarker = ({ coordinate, title, description }) => {
     <Marker
       coordinate={coordinate}
       image={mask}
-      title={title}
-      description={description}
-    />
+     // title={title}
+    // description={description}
+    >
+    <Callout>
+          <View style={{height: 120, width: 200}}>
+          <Text style={{
+          flex: 0,
+          color: "black",
+          fontSize: 20,
+          fontWeight: 'bold',
+          alignSelf: "flex-start",
+          paddingBottom: 0,
+          }}>{title} </Text>
+          <Text style ={{fontSize: 12 }}>{description} </Text>
+          </View>
+          </Callout>  
+    </Marker>
   );
 };
